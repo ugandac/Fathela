9a4efdcf0b73d9ccc687ccdeb7eb5c4813b58f70
Timetofficial <166425430+Timetofficial@users.noreply.github.com>
2025-02-20 14:30:33 +0300

    𝚳𝚪 𝚮𝚵𝚰𝐒𝚳𝚵 🎭-md

--- commandes/play.js
+++ commandes/play.js
@@ -11,7 +11,7 @@ zokou({
   nomCom: "play",
   aliases: ["song", "playdoc", "audio", "mp3"],
   categorie: "download",
-  reaction: "📍"
+  reaction: "🎥"
 }, async (dest, zk, commandOptions) => {
   const { arg, ms, repondre } = commandOptions;
 
@@ -133,7 +133,7 @@ zokou({
   nomCom: "video",
   aliases: ["videodoc", "film", "mp4"],
   categorie: "download",
-  reaction: "🔥"
+  reaction: "🎥"
 }, async (dest, zk, commandOptions) => {
   const { arg, ms, repondre } = commandOptions;
 
